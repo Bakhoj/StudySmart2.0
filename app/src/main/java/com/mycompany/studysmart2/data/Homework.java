d2d97216a01c1cd8bde87c485a36221051b8b0ce
anders <Anders2108@hotmail.com>
2015-11-23 21:22:37 +0100

    renamed homework

---
+++ app/src/main/java/com/mycompany/studysmart2/data/Homework.java
@@ -0,0 +1,33 @@
+package com.mycompany.studysmart2.data;
+
+import java.util.Date;
+
+/**
+ * Created by anders on 19-11-2015.
+ */
+public class Homework {
+    public Date date;
+    public int id, session, status;
+    public String title, description, shortdescription;
+
+    public Homework(int id, Date date, int session, String title, String description, String shortD, int status){
+        this.id = id;
+        this.date = date;
+        this.session = session;
+        this.title = title;
+        this.description = description;
+        this.shortdescription = shortD;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return title + "\n"
+                + date + "\n"
+                + shortdescription;
+    }
+
+    /*public String toListString() {
+        return (title + "\n" + shortdescription);
+    }*/
+}
