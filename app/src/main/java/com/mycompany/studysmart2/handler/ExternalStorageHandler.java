b7462b7d5cc60e100d5b9ca37010b0bfb613b8cd
LarsPede <larspeter94@gmail.com>
2015-12-07 01:29:04 +0100

    shitztorm

--- app/src/main/java/com/mycompany/studysmart2/handler/ExternalStorageHandler.java
+++ app/src/main/java/com/mycompany/studysmart2/handler/ExternalStorageHandler.java
@@ -1,9 +1,167 @@
 package com.mycompany.studysmart2.handler;
 
+import android.os.AsyncTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by anders on 26-11-2015.
  * External Storage handler
  * handles the communication to the database.
  */
 public class ExternalStorageHandler {
+    private final String serverUrl = "http://fuelshare.byethost.com/studySmart.php";
+    private String enteredEmail;
+    private String enteredPassword;
+
+    public void connectToDb() {
+        AsyncDataClass asyncRequestObject = new AsyncDataClass();
+
+        asyncRequestObject.execute(serverUrl, enteredEmail, enteredPassword);
+    }
+
+    private class AsyncDataClass extends AsyncTask<String, Void, String> {
+
+        @Override
+
+        protected String doInBackground(String... params) {
+
+            HttpParams httpParameters = new BasicHttpParams();
+
+            HttpConnectionParams.setConnectionTimeout(httpParameters, 5000);
+
+            HttpConnectionParams.setSoTimeout(httpParameters, 5000);
+
+            HttpClient httpClient = new DefaultHttpClient();
+
+            HttpPost httpPost = new HttpPost(params[0]);
+
+            String jsonResult = "";
+
+            try {
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+
+                nameValuePairs.add(new BasicNameValuePair("username", params[1]));
+
+                nameValuePairs.add(new BasicNameValuePair("password", params[2]));
+
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                HttpResponse response = httpClient.execute(httpPost);
+
+                jsonResult = inputStreamToString(response.getEntity().getContent()).toString();
+
+            } catch (ClientProtocolException e) {
+
+                e.printStackTrace();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+            return jsonResult;
+
+        }
+
+        @Override
+
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+        }
+
+        @Override
+
+        protected void onPostExecute(String result) {
+
+            super.onPostExecute(result);
+
+            System.out.println("Resulted Value: " + result);
+
+            if(result.equals("") || result == null){
+
+                return;
+
+            }
+
+            int jsonResult = returnParsedJsonObject(result);
+
+
+        }
+
+        private StringBuilder inputStreamToString(InputStream is) {
+
+            String rLine = "";
+
+            StringBuilder answer = new StringBuilder();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            try {
+
+                while ((rLine = br.readLine()) != null) {
+
+                    answer.append(rLine);
+
+                }
+
+            } catch (IOException e) {
+
+// TODO Auto-generated catch block
+
+                e.printStackTrace();
+
+            }
+
+            return answer;
+
+        }
+
+    }
+
+    private int returnParsedJsonObject(String result){
+
+        JSONObject resultObject = null;
+
+        int returnedResult = 0;
+
+        try {
+
+            resultObject = new JSONObject(result);
+
+            returnedResult = resultObject.getInt("success");
+
+        } catch (JSONException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return returnedResult;
+
+    }
+
 }
